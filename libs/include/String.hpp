18e9e6e5131cab0b706ff779b72903ee9e6b567b
erbo <shanerbo@gmail.com>
2021-03-27 20:39:42 -0700

    recreate project

---
+++ libs/include/String.hpp
@@ -0,0 +1,16 @@
+//
+// Created by shanerbo on 3/27/21.
+//
+
+#ifndef YET_ANOTHER_STL_STRING_HPP
+#define YET_ANOTHER_STL_STRING_HPP
+namespace YAS {
+    class String {
+    public:
+        explicit String() {
+        }
+
+    private:
+    };
+}// namespace YAS
+#endif//YET_ANOTHER_STL_STRING_HPP
---
+++ libs/src/String.cpp
@@ -0,0 +1,9 @@
+//
+// Created by shanerbo on 3/27/21.
+//
+
+#include "String.hpp"
+namespace YAS {
+    String::String() {
+    }
+}// namespace YAS
